3cd1760ea02c681708319c5a975dd93134b8bfe0
Markus-Rost <37275477+Markus-Rost@users.noreply.github.com>
2018-11-29 20:25:36 +0100

    added subemotes

--- app.js
+++ app.js
@@ -205,7 +205,13 @@ function bot_leave(channel, userstate, msg, args, wiki) {
 }
 
 function bot_link(channel, msg, title, wiki) {
-	var kit = ( channel == '#timewanderer33' ? 'timewaGGKit ' : '' );
+	switch (channel) {
+		case '#radicalfishgames':
+			var subemote = 'radica20LeaSmug ';
+			break;
+		default: 
+			var subemote = '';
+	}
 	request( {
 		uri: 'https://' + wiki + '.gamepedia.com/api.php?action=query&format=json&meta=siteinfo&siprop=general&iwurl=true&redirects=true&titles=' + encodeURI( title ),
 		json: true
@@ -213,7 +219,7 @@ function bot_link(channel, msg, title, wiki) {
 		if ( error || !response || !body || !body.query ) {
 			console.log( 'Fehler beim Erhalten der Suchergebnisse' + ( error ? ': ' + error.message : ( body ? ( body.error ? ': ' + body.error.info : '.' ) : '.' ) ) );
 			if ( response && response.request && response.request.uri && response.request.uri.href == 'https://www.gamepedia.com/' ) bot.say( channel, 'This wiki does not exist!' );
-			else bot.say( channel, kit + 'I got an error while searching: https://' + wiki + '.gamepedia.com/Special:Search/' + title.toTitle() );
+			else bot.say( channel, subemote + 'I got an error while searching: https://' + wiki + '.gamepedia.com/Special:Search/' + title.toTitle() );
 		}
 		else {
 			if ( body.query.pages ) {
@@ -224,26 +230,26 @@ function bot_link(channel, msg, title, wiki) {
 					}, function( srerror, srresponse, srbody ) {
 						if ( srerror || !srresponse || !srbody || !srbody.query || ( !srbody.query.search[0] && srbody.query.searchinfo.totalhits != 0 ) ) {
 							console.log( 'Fehler beim Erhalten der Suchergebnisse' + ( srerror ? ': ' + srerror.message : ( srbody ? ( srbody.error ? ': ' + srbody.error.info : '.' ) : '.' ) ) );
-							bot.say( channel, kit + 'I got an error while searching: https://' + wiki + '.gamepedia.com/Special:Search/' + title.toTitle() );
+							bot.say( channel, subemote + 'I got an error while searching: https://' + wiki + '.gamepedia.com/Special:Search/' + title.toTitle() );
 						}
 						else {
 							if ( srbody.query.searchinfo.totalhits == 0 ) {
-								bot.say( channel, kit + 'I couldn\'t find a result for "' + title + '" on this wiki :( https://' + wiki + '.gamepedia.com/' );
+								bot.say( channel, subemote + 'I couldn\'t find a result for "' + title + '" on this wiki :( https://' + wiki + '.gamepedia.com/' );
 							}
 							else if ( title.toTitle().toLowerCase() == srbody.query.search[0].title.toTitle().toLowerCase() ) {
-								bot.say( channel, 'https://' + wiki + '.gamepedia.com/' + srbody.query.search[0].title.toTitle() );
+								bot.say( channel, subemote + 'https://' + wiki + '.gamepedia.com/' + srbody.query.search[0].title.toTitle() );
 							}
 							else if ( srbody.query.searchinfo.totalhits == 1 ) {
-								bot.say( channel, kit + 'I found only this: https://' + wiki + '.gamepedia.com/' + srbody.query.search[0].title.toTitle() );
+								bot.say( channel, subemote + 'I found only this: https://' + wiki + '.gamepedia.com/' + srbody.query.search[0].title.toTitle() );
 							}
 							else {
-								bot.say( channel, kit + 'I found this for you: https://' + wiki + '.gamepedia.com/' + srbody.query.search[0].title.toTitle() );
+								bot.say( channel, subemote + 'I found this for you: https://' + wiki + '.gamepedia.com/' + srbody.query.search[0].title.toTitle() );
 							}
 						}
 					} );
 				}
 				else {
-					bot.say( channel, kit + 'https://' + wiki + '.gamepedia.com/' + Object.values(body.query.pages)[0].title.toTitle() + ( body.query.redirects && body.query.redirects[0].tofragment ? '#' + body.query.redirects[0].tofragment.toSection() : '' ) );
+					bot.say( channel, subemote + 'https://' + wiki + '.gamepedia.com/' + Object.values(body.query.pages)[0].title.toTitle() + ( body.query.redirects && body.query.redirects[0].tofragment ? '#' + body.query.redirects[0].tofragment.toSection() : '' ) );
 				}
 			}
 			else if ( body.query.interwiki ) {
@@ -253,10 +259,10 @@ function bot_link(channel, msg, title, wiki) {
 				if ( regex != null ) {
 					var iwtitle = decodeURIComponent( inter.url.replace( regex[0], '' ) ).replace( /\_/g, ' ' ).replace( intertitle.replace( /\_/g, ' ' ), intertitle );
 					bot_link(channel, msg, iwtitle, regex[1]);
-				} else bot.say( channel, kit + inter.url );
+				} else bot.say( channel, subemote + inter.url );
 			}
 			else {
-				bot.say( channel, kit + 'https://' + wiki + '.gamepedia.com/' + body.query.general.mainpage.toTitle() );
+				bot.say( channel, subemote + 'https://' + wiki + '.gamepedia.com/' + body.query.general.mainpage.toTitle() );
 			}
 		}
 	} );
