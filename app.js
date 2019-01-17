c160e8629077fe847f06fec2730932211f360519
Markus-Rost <sukramxro@gmail.com>
2019-01-17 12:36:48 +0100

    escape `,`

--- app.js
+++ app.js
@@ -266,7 +266,7 @@ function bot_link(channel, msg, title, wiki) {
 }
 
 String.prototype.toTitle = function() {
-	return this.replace( / /g, '_' ).replace( /\%/g, '%25' ).replace( /\'/g, '%27' ).replace( /\!/g, '%21' ).replace( /\?/g, '%3F' );
+	return this.replace( / /g, '_' ).replace( /\%/g, '%25' ).replace( /\,/g, '%2C').replace( /\'/g, '%27' ).replace( /\!/g, '%21' ).replace( /\?/g, '%3F' );
 };
 
 String.prototype.toSection = function() {
